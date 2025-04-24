0ff6fafbc777dfb8717113ed5993b3cd2b4e551a
Muhammed Aslam Shah S <muhammedaslamshahsofficial@gmail.com>
2025-04-24 14:27:56 -0700

    include validationhandleing.js to improve user interface from emply value login

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("views"));
 
-
-const email = "shah@shah.shah";
-const password = "1234";
-
-app.post("/login", (req, res) => {
-    console.log(req.body);
-    
-});
+const email_DB = "shah@shah.shah";
+const password_DB = "1234";
 
 const PORT = process.env.PORT || 3001;
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === email_DB && password === password_DB) {
+        res.send("<h1>Login SuccessFully</h1>");
+    } else {
+        res.send("<h1>Login failed</h1>");
+    }
+});
 
 app.listen(PORT);
